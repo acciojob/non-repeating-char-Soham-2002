ca90e12428ff66fde055915739f3daa82437dd62
Soham Kundu <sohamkundu29012002@gmail.com>
2025-04-17 12:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let k of str){
+		if(str.indexOf(k) == str.lastIndexOf(k)) return k;
+	}
+	return null;
 }
-const input = prompt("Enter a string");
+const input = prompt("Enter a string")
 alert(firstNonRepeatedChar(input)); 
